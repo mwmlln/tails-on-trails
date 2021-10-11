ab16fe9a32acb58a0dbbcbceecef683e8f5e4b05
mwmlln <mwmlln@gmail.com>
2021-10-11 15:09:10 +0000

    ADD commenting functionality for post_detail created

--- tails_on_trails/settings.py
+++ tails_on_trails/settings.py
@@ -56,6 +56,7 @@ INSTALLED_APPS = [
     'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
+    'crispy_forms',
     'forum',
 ]
 
@@ -64,6 +65,8 @@ SITE_ID = 1
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 LOGIN_URL = 'home'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
