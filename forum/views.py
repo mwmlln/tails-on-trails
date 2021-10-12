609e759e776acb70e7cc0d6025219f28baed2a07
mwmlln <mwmlln@gmail.com>
2021-10-12 10:25:00 +0000

    ADD: Likes button can be pressed iand update Likes number in post_detail page now

--- forum/views.py
+++ forum/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, reverse
 from django.views import generic, View
 from django.utils.decorators import method_decorator
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect
 from .models import Post
 from .forms import CommentForm
 
@@ -72,5 +73,17 @@ class PostDetail(View):
                 "liked": liked
             },
         )
+
+    
+class PostLike(View):
+    
+    def post(self, request, slug, *args, **kwargs):
+        post = get_object_or_404(Post, slug=slug)
+        if post.likes.filter(id=request.user.id).exists():
+            post.likes.remove(request.user)
+        else:
+            post.likes.add(request.user)
+
+        return HttpResponseRedirect(reverse('post_detail', args=[slug]))
 
 
