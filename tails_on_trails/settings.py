dcb0d64d2f2a719f9a28087aedc1a47ee2f71897
mwmlln <mwmlln@gmail.com>
2021-10-26 18:01:54 +0000

    deployment commit with DEBUG false as failing to save before submission in the prior commit

--- tails_on_trails/settings.py
+++ tails_on_trails/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-
+git 
 X_FRAME_OPTIONS = 'SAMEORIGIN'
 
 ALLOWED_HOSTS = ['tailsontrails.herokuapp.com', 'localhost']
