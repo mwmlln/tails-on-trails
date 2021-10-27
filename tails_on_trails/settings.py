38c6567708ef46b2170277feb536df63ea378d98
mwmlln <mwmlln@gmail.com>
2021-10-27 08:15:29 +0000

    DOCUMENTATION: x_frame_option commented out temporarily to get mockup screenshot

--- tails_on_trails/settings.py
+++ tails_on_trails/settings.py
@@ -29,9 +29,9 @@ STATIC_DIR = os.path.join(BASE_DIR, 'static')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-X_FRAME_OPTIONS = 'SAMEORIGIN'
+# X_FRAME_OPTIONS = 'SAMEORIGIN'
 
 ALLOWED_HOSTS = ['tailsontrails.herokuapp.com', 'localhost']
 
