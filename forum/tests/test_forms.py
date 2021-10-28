a2a6c4d383f64061e3409fbc0e8d6de9be044fdf
mwmlln <mwmlln@gmail.com>
2021-10-28 09:24:38 +0000

    TEST: testing forms added but still in progress

--- forum/tests/test_forms.py
+++ forum/tests/test_forms.py
@@ -1,52 +1,69 @@
-# from django.test import SimpleTestCase, TestCase
-# from forum.forms import CommentForm, CreatePostForm, EditPostForm
-# from forum.forms import DeletePostForm,  ProfileEditForm
+from django.test import SimpleTestCase, TestCase
+from forum.forms import CommentForm, CreatePostForm, EditPostForm
+from forum.forms import DeletePostForm,  ProfileEditForm, Comment
 
 
-# class CreatePostForm(TestCase):
+class TestCreatePostForm(TestCase):
 
-#     def test_title_entry_is_required(self):
-#         form = CreatePostForm({'title': ''})
-#         self.assertFalse(form.is_valid())
-#         self.assertIn('title', form.errors.keys())
-#         self.assertEqual(form.errors['title'][0], 'This fiels is required.')
+    def test_title_entry_is_required(self):
+        form = CreatePostForm({'title': ''})
+        self.assertFalse(form.is_valid())
+        self.assertIn('title', form.errors.keys())
+        self.assertEqual(form.errors['title'][0], 'This field is required.')
 
-#     def test_location_field_is_not_required(self):
-#         form = CreatePostForm({'title': 'test title'})
-#         self.assertTrue(form.is_valid())
+    def test_excerpt_field_is_not_required(self):
+        form = CreatePostForm({
+                            'title': 'test title', 
+                            'slug': 'test-title',
+                            'author': 'user_a',
+                            'location' : 'test',
+                            'content': 'test',
+                            'excerpt': ''})
+        self.assertTrue(form.is_valid())
 
-#     def test_fields_are_explicit_in_form_mataclass(self):
-#         form = CreatePostForm()
-#         self.assertEqual(form.Mata.fields, [
-#                                         'title',
-#                                         'location',
-#                                         'excerpt',
-#                                         'featured_image',
-#                                         'content',
-#                                         'difficulty_hard',
-#                                         'difficulty_moderate',
-#                                         'difficulty_easy',
-#                                         'breed_big',
-#                                         'breed_mid',
-#                                         'breed_sml'
-#                                     ])
+    # def test_fields_are_explicit_in_form_mataclass(self):
+    #     form = CreatePostForm()
+    #     self.assertEqual(form.Meta.fields, [
+    #                                     'title',
+    #                                     'location',
+    #                                     'excerpt',
+    #                                     'featured_image',
+    #                                     'content',
+    #                                     'difficulty_hard',
+    #                                     'difficulty_moderate',
+    #                                     'difficulty_easy',
+    #                                     'breed_big',
+    #                                     'breed_mid',
+    #                                     'breed_sml'
+    #                                 ])
+
+class TestCommentForm(TestCase):
+    def test_fields_are_explicit_in_form_metaclass(self):
+        form = CommentForm()
+        self.assertEqual(form.Meta.fields, ('body',))
 
 
-# # class TestForms(SimpleTestCase):
+class TestEditPostForm(TestCase):
 
-# #     def test_create_post_valid_data(self):
-# #         form = CreatePostForm(data={
-# #             'title': 'my post',
-# #             'location' : 'test location',
-# #             'content' : 'my test post'
-# #         })
+    def test_title_entry_is_required(self):
+        form = CreatePostForm({'title': ''})
+        self.assertFalse(form.is_valid())
+        self.assertIn('title', form.errors.keys())
+        self.assertEqual(form.errors['title'][0], 'This field is required.')
 
-# #         self.assertTrue(form.is_valid())
+    def test_excerpt_field_is_not_required(self):
+        form = CreatePostForm({
+                            'title': 'test title', 
+                            'slug': 'test-title',
+                            'author': 'user_a',
+                            'location' : 'test',
+                            'content': 'test',
+                            'excerpt': ''})
+        self.assertTrue(form.is_valid())
 
-# #     def test_create_post_no_data(self):
-# #         form = CreatePostForm(data={})
 
-# #         self.assertFalse(form.is_valid())
-# #         self.assertEquals(len(form.errors), 3)
+class TestProfileEditForm(TestCase):
+    
+
 
 
