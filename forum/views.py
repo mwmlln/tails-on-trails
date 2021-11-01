ef8d1afae1346257492d7d66740f293820e06103
mwmlln <mwmlln@gmail.com>
2021-11-01 12:13:34 +0000

    FIX: corrections made to avoid form error messages to passed on to post list page

--- forum/views.py
+++ forum/views.py
@@ -113,7 +113,8 @@ def create_post(request):
     return render(
                 request,
                 'post_create.html',
-                context={'create_post_form': create_post_form}
+                context={
+                    'create_post_form': create_post_form}
                 )
 
 
@@ -134,9 +135,13 @@ def edit_post(request, slug):
             messages.success(request, 'Your post is successfully updated')
             return redirect('posts')
         else:
-            messages.error(request,
-                           ('Update failed. Please ensure'
-                            'the form is valid.'))
+            messages.error(request, 'Please ensure to enter required fields')
+            return render(
+                        request,
+                        'post_edit.html',
+                        context={
+                        'edit_post_form': edit_post_form, 'slug': slug}
+                        )
     else:
         edit_post_form = EditPostForm(instance=post)
     template = 'post_edit.html'
