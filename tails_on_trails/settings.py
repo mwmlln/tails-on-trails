047fdfcbd107056f5c02742f96d36c466dfc2139
mwmlln <mwmlln@gmail.com>
2022-11-22 10:24:05 +0000

    Setting update to migrate to Render

--- tails_on_trails/settings.py
+++ tails_on_trails/settings.py
@@ -33,7 +33,13 @@ DEBUG = False
 
 X_FRAME_OPTIONS = 'SAMEORIGIN'
 
+
 ALLOWED_HOSTS = ['tailsontrails.herokuapp.com', 'localhost']
+
+# Add Render.com URL to allowed hosts
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 # Setting to accomodate testing on local DB
 development = os.environ.get('DEVELOPMENT', False)
